2072071b852e26ff7792da9ec909e6d0219d75ff
Anthony_Yu <42222043+Anthony9624@users.noreply.github.com>
2020-07-20 09:06:19 +0800

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,5 +1,6 @@
-INSERT INTO `student` VALUES (172230203, '田一卉', '123456', '计算机1702', '女');
-INSERT INTO `student` VALUES (173230202, '王昊天', '222', '计算机1702', '女');
-INSERT INTO `student` VALUES (173230210, '桃桃', '111', '计算机1702', '女');
-INSERT INTO `student` VALUES (1514010924, '于强', '456253', '15140X04', '男');
-INSERT INTO `student` VALUES (1514010926, '张三', '1', '2班', '男');
+INSERT INTO `student` VALUES (192230203, '小王', '123456', '计信19', '女');
+INSERT INTO `student` VALUES (193230202, '小陈', '22287', '计信19', '男');
+INSERT INTO `student` VALUES (193230210, '小傅', '111522', '计信19', '男');
+INSERT INTO `student` VALUES (1914010924, '于强', '456253', '15140X04', '男');
+INSERT INTO `student` VALUES (1914010926, '张三', '1', '2班', '男');
+INSERT INTO `student` VALUES (193230213, '小冯', '1115272', '计信19', '男');
