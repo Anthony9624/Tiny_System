396825fd05221830bfa18040f21552ed230e245e
Anthony_Yu <42222043+Anthony9624@users.noreply.github.com>
2020-07-20 09:07:39 +0800

    Update teacher.sql

--- teacher.sql
+++ teacher.sql
@@ -1,3 +1,5 @@
-INSERT INTO `teacher` VALUES (1, '张森玮', '111');
+INSERT INTO `teacher` VALUES (1, '张晶', '111');
 INSERT INTO `teacher` VALUES (2, '郭丹', '222');
-INSERT INTO `teacher` VALUES (3, '李淑弈', '333');
+INSERT INTO `teacher` VALUES (3, '李长虹', '333');
+INSERT INTO `teacher` VALUES (4, '王大鹏', '444');
+INSERT INTO `teacher` VALUES (5, '张大伟', '555');
